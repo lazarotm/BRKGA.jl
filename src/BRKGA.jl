eecd73ce3be126e0d699e31735d5f34c9328f7dd
lazarotm <lljs@cin.ufpe.br>
2017-06-28 12:00:16 -0300

    main nao exportada

--- src/BRKGA.jl
+++ src/BRKGA.jl
@@ -8,7 +8,7 @@ include("brkga_main.jl")
 include("brkga_parser.jl")
 
 #functions and variables exported
-export main,n, p, pe, pm, rhoe, K, MAXT, generation,
+export n, p, pe, pm, rhoe, K, MAXT, generation,
 X_INTVL, X_NUMBER, MAX_GENS, N_Evolutions
 
 n=2
